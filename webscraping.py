15099b81585ca637e005c48685499a4d0011c34a
mah5a <46025103+mah5a@users.noreply.github.com>
2024-07-17 20:51:14 -0700

    soup-attrs

--- webscraping.py
+++ webscraping.py
@@ -16,3 +16,17 @@ first_header = soup.find("h2")
 print(first_header)
 headers = soup.find_all("h2")
 print(headers)
+
+# pass in a list of elements to look for the first element of the list
+print("--------------")
+find_header = soup.find(["h1", "h2"])
+print(find_header)
+print("--------------")
+# pass in a list of elements to look for all
+headers = soup.find_all(["h1", "h2"])
+print(headers)
+print("--------------")
+# pass in attributes to find/find_all
+paragraph = soup.find_all("p", attrs={"id": "paragraph-id"})
+print(paragraph)
+
